4a093d6dafe1395503c5164e19016f3a2e6c246e
fergus-g <fergus.gildea@utrack.com>
2024-12-04 13:39:45 +0000

    Working on Ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -3,10 +3,29 @@ function countNumberOfNinesInNumber(number) {
 
   for (let i = 0; i < number.length; i++) {
     const digit = number[i];
-    if ("9" === digit) {
+    
+    if (9 === digit) {
       count++;
     }
   }
-
+  
   return count;
 }
+
+// Enter a number
+//    How do we count the numbers?
+//    Split number into individual numbers and check how many are 9s
+// Count how many 9s are in that number
+// return that number by adding to the count
+
+// Working attempt
+// const numberString = number.toString();
+// const digits = [];
+// for (let i = 0; i < numberString.length; i++) {
+//   digits.push(Number(numberString.charAt(i)))
+//   console.log(digits)
+  
+//   if ('9' === digits) {
+//     count++;
+//   }
+// }
